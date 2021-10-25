de50c7599fa74e48621399afbef0da9e6d6fcb30
Matvii <mtw.karp@gmail.com>
2021-10-25 21:00:15 +0300

    buttons ready

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
+const {gameOptions, againOptions} = require('./options')
 const TelegramApi = require('node-telegram-bot-api');
 const TOKEN = '2049468452:AAG8FSNiGNcH3IzfSaOGUObNEDDxNqL9Frc'
 
 const bot = new TelegramApi(TOKEN, {polling: true})
-const chats = { }
+const chats = {}
+
+const startGame = async (chatId) => {
+    await bot.sendMessage(chatId, 'Сейчас я загадаю тебе цифру от нуля до 9!')
+
+    const randomNum = Math.floor(Math.random() * 10)
+
+    chats[chatId] = randomNum
+
+    await bot.sendMessage(chatId, 'Отгадывай, сука !' + randomNum, gameOptions)
+}
 
 const start = async () => {
-    bot.setMyCommands([
+    await bot.setMyCommands([
         {command: '/start', description: 'Прив'},
         {command: '/game', description: 'Поиграть в игру'}
     ])
@@ -19,18 +30,30 @@ const start = async () => {
         }
 
         if(text === '/game') {
-            await bot.sendMessage(chatId, 'Сейчас я загадаю тебе цифру от нуля до 9!')
-
-            const randomNum = Math.floor(Math.random() * 10)
-
-            chats[chatId = randomNum]
-
-            return bot.sendMessage(chatId, 'Отгадывай, сука !')
+            startGame(chatId)
         }
     
         return bot.sendMessage(chatId, 'Я тебя не понимать !') 
-        console.log(msg)
+        // console.log(msg)
+    })
+
+    bot.on('callback_query', async msg => {
+        const {data, message} = msg,
+              chatId = message.chat.id;
+
+        if(data === '/again') {
+            startGame(chatId)
+        }
+
+        if(Number(data) === chats[chatId]) {
+            return await bot.sendMessage(chatId, `Правильно!`, againOptions)
+
+        }else {
+            return await bot.sendMessage(chatId, 'Не правильно !', againOptions)
+
+        }
+
     })
 }
 
-start()
+start();
